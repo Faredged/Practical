e89445ca0d3ba7f49041a9f61aa148d7003a36c0
Faredged <91426604+Faredged@users.noreply.github.com>
2024-09-04 09:19:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
+/**
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ */
 function add(a, b) {
     return a + b;
 }
 
+/**
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ */
 function subtract(a, b) {
     return a - b;
 }
 
+/**
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ */
 function multiply(a, b) {
     return a * b;
 }
 
+/**
+ * @param {number} a
+ * @param {number} b
+ * @returns {number | null}
+ */
 function divide(a, b) {
     if (b === 0) {
         return null;
@@ -17,183 +37,11 @@ function divide(a, b) {
     return a / b;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/** @type {number} */
 const num1 = 10;
+/** @type {number} */
 const num2 = 5;
+
 console.log(`Add: ${add(num1, num2)}`);
 console.log(`Subtract: ${subtract(num1, num2)}`);
 console.log(`Multiply: ${multiply(num1, num2)}`);
